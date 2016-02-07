0f86a786106d0eb1f17a9a5004dae5e3ea6964d8
Michael Porter <m_porter@live.co.uk>
2016-02-07 23:05:11 +0000

    Added header to nav bar

--- app/src/main/java/com/michael/kidquest/MainActivity.java
+++ app/src/main/java/com/michael/kidquest/MainActivity.java
@@ -6,72 +6,71 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.michael.kidquest.model.Quest;
 
 public class MainActivity extends AppCompatActivity implements QuestLogFragment.OnListFragmentInteractionListener {
 
-    private String[] mNavigationDrawerItemTitles;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ArrayAdapter<String> mAdapter;
-    private ActionBarDrawerToggle mDrawerToggle;
+    public RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * Toolbar setup
+         * */
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Your Quests");
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //toolbar.setNavigationIcon(R.);
-
-        mNavigationDrawerItemTitles = getResources().getStringArray(R.array.navigation_drawer_items);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                mDrawerLayout,
-                toolbar,
-                R.string.drawer_open,
-                R.string.drawer_close
-        );
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open,
+                R.string.drawer_close);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
 
 
-        addDrawerItems();
+        String[] mNavigationDrawerItemTitles = getResources().getStringArray(R.array.navigation_drawer_items);
+        RecyclerView mDrawerList = (RecyclerView) findViewById(R.id.left_drawer);
+        mDrawerList.setHasFixedSize(true);
+
+        NavBarListAdapter mAdapter = new NavBarListAdapter(mNavigationDrawerItemTitles, "Balthazaro", 18);
+        mDrawerList.setAdapter(mAdapter);
+
+        LinearLayoutManager mLayoutManager2 = new LinearLayoutManager(this);
+        mDrawerList.setLayoutManager(mLayoutManager2);
+
 
         Fragment fragment = new QuestLogFragment();
-
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 
     }
 
-    private void addDrawerItems(){
-        String[] navArray = {"Open Quests", "Pending Quests"};
-        mAdapter = new ArrayAdapter<String>(this, R.layout.drawer_list_item, navArray);
-        mDrawerList.setAdapter(mAdapter);
+    private void addDrawerItems() {
 
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Fragment fragment = new QuestLogFragment();
 
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-            }
-        });
+
+
+/**
+ mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+@Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+Fragment fragment = new QuestLogFragment();
+
+FragmentManager fragmentManager = getFragmentManager();
+fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+}
+});
+
+ */
     }
 
     @Override
