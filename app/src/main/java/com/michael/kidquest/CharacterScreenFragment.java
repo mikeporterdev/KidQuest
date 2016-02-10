a45d75a09bff3205ab3ed7dc1af2663bb7467c74
Michael Porter <m_porter@live.co.uk>
2016-02-10 10:52:53 +0000

    User is now prompted for character name at first startup

--- app/src/main/java/com/michael/kidquest/CharacterScreenFragment.java
+++ app/src/main/java/com/michael/kidquest/CharacterScreenFragment.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.michael.kidquest.greendao.model.*;
+import com.michael.kidquest.greendao.model.Character;
 
 /**
  * Created by Michael Porter on 09/02/16.
@@ -15,6 +19,16 @@ public class CharacterScreenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_character_screen, container, false);
+
+        DaoSession daoSession = ((KidQuestApplication) view.getContext().getApplicationContext()).getDaoSession();
+        Character character = daoSession.getCharacterDao().loadAll().get(0);
+
+        TextView txtName = (TextView) view.findViewById(R.id.character_name);
+        txtName.setText(character.getName());
+
+        TextView txtLevel = (TextView) view.findViewById(R.id.character_level);
+        txtLevel.setText("Level: " + String.valueOf(character.getLevel()));
+
         return view;
     }
 }
--- app/src/main/java/com/michael/kidquest/MainActivity.java
+++ app/src/main/java/com/michael/kidquest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.michael.kidquest;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -10,13 +12,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.michael.kidquest.greendao.model.Character;
+import com.michael.kidquest.greendao.model.DaoSession;
 import com.michael.kidquest.greendao.model.Quest;
 import com.michael.kidquest.quest.AddQuestActivity;
 import com.michael.kidquest.quest.OpenQuestLogFragment;
 import com.michael.kidquest.quest.PendingQuestLogFragment;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OpenQuestLogFragment.OnListFragmentInteractionListener, PendingQuestLogFragment.OnListFragmentInteractionListener {
     private DrawerLayout mDrawerLayout;
@@ -24,17 +32,26 @@ public class MainActivity extends AppCompatActivity implements OpenQuestLogFragm
     private FragmentManager mFragmentManager;
 
     private final static int ADD_QUEST_CODE = 2;
+    private String mCharacterNameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DaoSession daoSession = ((KidQuestApplication) this.getApplicationContext()).getDaoSession();
+
+        //TODO: change this to not use a list
+        List<Character> character = daoSession.getCharacterDao().loadAll();
+        //daoSession.getCharacterDao().delete(character.get(0));
+        if (character.size() == 0){
+            firstTimeSetup();
+        } else {
+            initialFragmentSetup();
+        }
+
         toolbarSetup();
         sidebarSetup();
-        initialFragmentSetup();
-
-
     }
 
     private void initialFragmentSetup() {
@@ -115,5 +132,36 @@ public class MainActivity extends AppCompatActivity implements OpenQuestLogFragm
             fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
             mDrawerLayout.closeDrawers();
         }
+    }
+
+    public void firstTimeSetup(){
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Character Name");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Character character = new Character();
+
+                mCharacterNameInput = input.getText().toString();
+
+                character.setName(mCharacterNameInput);
+                character.setLevel(1);
+                character.setParentPin("1066");
+
+                DaoSession daoSession = ((KidQuestApplication) getApplicationContext()).getDaoSession();
+                daoSession.getCharacterDao().insertOrReplace(character);
+
+                initialFragmentSetup();
+            }
+        });
+
+        builder.show();
     }
 }
